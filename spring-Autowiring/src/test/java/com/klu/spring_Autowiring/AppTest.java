76c2601645902ccd0bc58655070973be95ecd78c
fayaz-shaik17 <fayazshaik17102006@gmail.com>
2026-02-19 15:00:46 +0530

    Add files via upload

---
+++ spring-Autowiring/src/test/java/com/klu/spring_Autowiring/AppTest.java
@@ -0,0 +1,13 @@
+package com.klu.spring_Autowiring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
